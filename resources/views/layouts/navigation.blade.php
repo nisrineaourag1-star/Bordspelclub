f9918a29fe612d23f9f2a0d40a4e7ab59e5cae72
nisrineaourag1-star <nisrineaourag1@gmail.com>
2026-08-26 22:02:08 +0200

    Navigatiemenu uitgebreid met links naar nieuws, faq, events en contact

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,6 +12,18 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('news.index')" :active="request()->routeIs('news.*')">
+                        Nieuws
+                    </x-nav-link>
+                    <x-nav-link :href="route('faq.index')" :active="request()->routeIs('faq.index')">
+                        FAQ
+                    </x-nav-link>
+                    <x-nav-link :href="route('events.index')" :active="request()->routeIs('events.*')">
+                        Events
+                    </x-nav-link>
+                    <x-nav-link :href="route('contact.create')" :active="request()->routeIs('contact.*')">
+                        Contact
+                    </x-nav-link>
                     @auth
                         <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                             {{ __('Dashboard') }}
@@ -76,6 +88,18 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('news.index')" :active="request()->routeIs('news.*')">
+                Nieuws
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('faq.index')" :active="request()->routeIs('faq.index')">
+                FAQ
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('events.index')" :active="request()->routeIs('events.*')">
+                Events
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('contact.create')" :active="request()->routeIs('contact.*')">
+                Contact
+            </x-responsive-nav-link>
             @auth
                 <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                     {{ __('Dashboard') }}
@@ -119,4 +143,4 @@
             </div>
         @endauth
     </div>
-</nav
+</nav>
